7fa5f7a36bca5140533ead0b6773aef4207b9cd0
DDemmer1 <Dennis.Demmer@web.de>
2022-04-12 11:29:20 +0200

    add Hund

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,10 +1,38 @@
 package de.demmer.dennis;
 
+import java.util.ArrayList;
+
 public class Application {
 	
 	public static void main(String[] args) {
-		//sasa
-		System.out.println("Hello World");
-	}
 
+		//STRG + SHIFT + O -> Auto Import
+		
+		Hund hund1 = new Hund(10, "Bello");
+		Hund hund2 = new Hund(10, "Carlo");
+		Hund hund3 = new Hund(10, "Klausi");
+		Hund hund4 = new Hund(10, "Wuffi");
+
+		ArrayList<Hund> hundListe = new ArrayList<Hund>();
+		hundListe.add(hund1);
+		hundListe.add(hund2);
+		hundListe.add(hund3);
+		hundListe.add(hund4);
+
+		
+		
+		for(Hund h : hundListe) {
+			System.out.println(h.getName());
+			System.out.println(h.getAge());
+			System.out.println("***********");
+		}
+				
+		System.out.println(hundListe.size());
+
+		hundListe.clear();
+		
+		
+		
+	}
+	
 }
